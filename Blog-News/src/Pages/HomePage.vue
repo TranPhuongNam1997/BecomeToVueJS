ec4927b32c45efa16ff4d6c1780247ecf0e4d2e3
TranPhuongNam <tranphuongnam17041997@gmail.com>
2021-06-23 17:53:02 +0700

    update

--- Blog-News/src/Pages/HomePage.vue
+++ Blog-News/src/Pages/HomePage.vue
@@ -1,8 +1,9 @@
 <template>
     <div>
         <slider/>
-        <router-view></router-view>
-
+        <transition>
+            <router-view></router-view>
+        </transition>
     </div>
 </template>
 
@@ -15,5 +16,28 @@ export default {
 </script>
 
 <style>
+/* cái clas v- này là mặc định có thể thay đổi bằng cách đặt name cho nó */
+  .v-enter-active,.v-leave-active{
+    transition: all 0.3s ease;
+  }
+  /* Component bắt đầu vào */
 
+  .v-enter{
+    opacity: 0;
+    transform: scale(1.1);
+  }
+  .v-enter-to{
+    opacity: 1;
+    transform: scale(1);
+  }
+  /* Component bắt đầu ra */
+  
+  .v-leave{
+    opacity: 0;
+    transform: scale(1.1);
+  }
+  .v-leave-to{
+    opacity: 1;
+    transform: scale(1);
+  }
 </style>
--- Blog-News/src/main.js
+++ Blog-News/src/main.js
@@ -1,86 +1,14 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
 import App from './App.vue'
 import Vuex from 'vuex'
-// import page
-import HomePage from './Pages/HomePage.vue'
-import Home from './Pages/Home.vue'
-import AboutUs from './Pages/AboutUs.vue'
-import Login from './Pages/Login.vue'
-import ListUser from './Pages/ListUser.vue'
-import NotFound from './Pages/Notfound.vue'
 
+import router from './router'
 
 import './plugins'
-import { LayoutPlugin } from 'bootstrap-vue'
 
 Vue.use(Vuex)
-Vue.use(VueRouter)
 
 
-let islogin = true;
-
-const routes = [
-  { 
-    path: '/', 
-    component: HomePage,
-
-    children:[
-      {
-        path: '', 
-        component: Home,
-      },
-      {
-        path: 'aboutus', 
-        component: AboutUs,
-      }
-    ]
-  },
-  {
-    path: '/login',component: Login
-  },
-  
-  {
-    // khi mà để dấu hỏi thì vue sẽ tự hiểu là không cần id cũng được
-    path: '/user/:id?',
-    component: ListUser,
-    beforeEnter: (to, from, next) => {
-      // nếu đăng nhập rồi thì chạy tiếp
-      if(islogin){
-        next();
-      }
-
-      //nếu chưa đăng nhập thì chạy về login
-      else{
-        next({
-          path: '/login',
-          // cái query này có tác dụng thể hiện phân biệt so với login     /login?ridirect=user
-          query: { ridirect:'user'}
-        })
-      }
-    }
-  },
-
-  //nếu như đường dẫn không thuộc bất cứ đường dẫn nào trong những cái trên thì sẽ vào cái * này 404
-
-  {
-    path: '*',
-    component: NotFound
-  }
-
-]
-
-const router = new VueRouter({
-  mode: 'history', // để không phải dùng /#/ nữa
-  routes // short for `routes: routes`
-})
-
-router.beforeEach((to, from, next) => {
-  console.log('beforeEach to',to)
-  console.log('beforeEach from',from)
-  console.log('beforeEach next',next);
-  next();
-})
 new Vue({
   el: '#app',
   router,
